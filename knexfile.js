842ba3014dacb5445de74aa4433638dc97aaf765
Quinne Farenwald <q.farenwald@gmail.com>
2019-12-01 14:13:58 -0700

    Edit knex file with production code

--- knexfile.js
+++ knexfile.js
@@ -3,5 +3,10 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/favorites_tracker',
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    useNullAsDefault: true
   }
 };
